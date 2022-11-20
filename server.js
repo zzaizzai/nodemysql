557d091798157cfe91bf453cbcc280c3fea448a7
zzaizzai <junsai0530@gmail.com>
2022-11-21 01:22:50 +0900

    dd

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 const bodyParser = require('body-parser');
 require('dotenv').config()
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+app.use('/public', express.static('public'))
 
 
 const con = mysql.createConnection({
@@ -21,14 +23,29 @@ con.connect((err) => {
     console.log('success');
     con.query('CREATE DATABASE nodejs', function (err, result) {
         // if (err) throw err;
-        const sql = 'CREATE TABLE users (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL)';
+        const sql = "CREATE TABLE users (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL);"
+
         con.query(sql, function (err, result) {
-            // if (err) throw err;
-            // console.log('table created');
+            if (err) {
+
+            } else {
+                console.log('created users table');
+            }
+
+        });
+
+        const sql2 = "CREATE TABLE works (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, title VARCHAR(255) NOT NULL, FOREIGN KEY (user_id) REFERENCES users (id) ); "
+
+        con.query(sql2, function (err, result) {
+            if (err) {
+
+            } else {
+                console.log('created works table');
+            }
+
         });
     });
 });
-
 
 
 app.listen(8080, function () {
@@ -37,7 +54,7 @@ app.listen(8080, function () {
 
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/templates/index.html')
+    res.render('index.ejs')
 })
 
 
@@ -46,7 +63,7 @@ app.get('/test', function (req, res) {
 })
 
 app.get('/write', (req, res) => {
-    res.sendFile(__dirname + '/templates/write.html')
+    res.render('write.ejs')
 });
 
 app.post('/write/add', (req, res) => {
@@ -61,26 +78,51 @@ app.post('/write/save', (req, res) => {
 })
 
 app.get('/users', (req, res) => {
-    con.connect((err) =>{
+    con.connect((err) => {
         console.log('search users data');
         const sql = "select * from users"
-        con.query(sql, function(err, result, fields) {
+        con.query(sql, function (err, result, fields) {
             if (err) throw err;
             console.log(result)
+            res.render('users.ejs', { users: result })
         })
-        
+
     })
-    res.send('done')
+})
+
+app.get('/users/:id', (req, res) => {
+    console.log(req.params.id)
+    var user_id = parseInt(req.params.id)
+    const sql = `select * from users where users.id = ${user_id} ;` 
+    console.log(sql)
+    con.query(sql, function (err, result) {
+        // if (err) throw err;
+        console.log(result)
+        res.render('users_detail.ejs', { users: result })
+
+    })
+})
+
+app.delete('/users/delete', (req, res) => {
+    console.log(req.body)
+    req.body.id = parseInt(req.body.id)
+    const sql = `delete from users where id = ${req.body.id}`
+    con.query(sql, (err, result, fields) => {
+        if (err) throw err;
+        console.log('deleted')
+        res.status(200).send({ message: "success" })
+    })
 })
 
 app.post('/users/add', (req, res) => {
     console.log(req.body)
-    con.connect(function(err){
+    con.connect(function (err) {
         console.log('insert users data');
         const sql = `insert into users(name, email) values("${req.body.name}","${req.body.email}")`
-        con.query(sql, (err, result, fields)=>{
+        con.query(sql, (err, result, fields) => {
             if (err) throw err;
+            res.send(result)
         })
     })
-    res.send('done')
+
 })
